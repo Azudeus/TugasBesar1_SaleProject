1d357f49a624fbe66d27b6659f458bdf0b5ba3ca
Azudeus <blitzthunder68@yahoo.com>
2016-09-21 08:47:31 +0700

    Added Sales Table

--- sales.php
+++ sales.php
@@ -17,5 +17,30 @@
 </table>
 <p>this is sales</p>
 
+<table class="salestable">
+	<tr>
+		<th colspan="3"> Here are your sales </td>
+	</tr>
+	<tr>
+		<th colspan="3">
+			<br><br>
+			<p class="Date"> Sunday, 17 September  </p>
+			<p class="Time"> at 20.00 </p>
+		</th>
+	<tr>
+		<td>
+			<img src="airstyle.png">
+		</td>
+		<td>
+			<p class="sales_title"> Phelps Air Styler </p>
+			<p class="sales_price"> IDR 1.000.000 <br> 2 pcs <br> @IDR500.000 </p>
+		</td>
+		<td>
+			<p class="sales_desc"> Delivery to Evangeline <br> Jalan cibening barat no 20. <br> Bandung <br> 40280 <br> 08132933726 </p>
+		</td>
+	</tr>
+</table>
+		
+
 </body>
 </html>
