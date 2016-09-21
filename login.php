ccdbdae67021de985d86c8de9efb7ac58e266d7d
Azudeus <blitzthunder68@yahoo.com>
2016-09-21 08:15:32 +0700

    Initial Login Page

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<h1><center> SaleProject </h1></center><br>
+<p> Please Login </p>
+
+<form action = "welcome.php" method ="post">
+
+Email or Username <br>
+<input type="text" name="username"><br><br>
+
+Password <br>
+<input type="password" name="password"><br><br>
+
+Register<br>
+<input type="submit" value="LOGIN"><br>
+</form>
+<br><br>
+
+<p>Don't have an account yet? Register
+<a href="register.php">here</a>
+<br><br>
