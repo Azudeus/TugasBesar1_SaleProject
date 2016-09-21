5aee4c4394cde494ee11e4afeae04b60c57ed4de
Azudeus <blitzthunder68@yahoo.com>
2016-09-21 08:05:55 +0700

    Initial Register Page

---
+++ register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<h1><center> SaleProject </h1></center><br>
+<p> Please Register </p>
+
+<form action = "welcome.php" method ="post">
+
+Full Name <br>
+<input type="text" name="name"><br><br>
+
+Username <br>
+<input type="text" name="username"><br><br>
+
+Email <br>
+<input type="text" name="email"><br><br>
+
+Password <br>
+<input type="text" name="password"><br><br>
+
+Confirm Password <br>
+<input type="password" name="password"><br><br>
+
+Full Address <br>
+<textarea rows="3" cols="50" name="address"></textarea><br><br>
+
+Postal Code<br>
+<input type="text" name="postal"><br><br>
+
+Phone Number<br>
+<input type="number" name="number"><br><br>
+
+Register<br>
+<input type="submit" value="Register"><br>
+</form>
+<br><br>
+
+<p>Already registered? Login 
+<a href="login.php">here</a>
+<br><br>
