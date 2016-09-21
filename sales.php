ac18375560ff6d0e636010744bf54b7631b1ea9a
rellons <yeksa.ningrat@gmail.com>
2016-09-21 08:10:54 +0700

    hyperlink table

---
+++ sales.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="catalogtable.css">
+</head>
+<body>
+<table>
+	<tr>
+		<td><a href="catalog.php">catalog</a></td>
+		<td><a href="yourproduct.php">your product</a></td>
+		<td><a href="addproduct.php">add product</a></td>
+		<td><a href="sales.php">sales</a></td>
+		<td><a href="purchases.php">purchases</a></td>
+	</tr>
+
+</table>
+<p>this is sales</p>
+
+</body>
+</html>
