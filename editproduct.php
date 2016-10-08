1517547ff4818b6f0f7261a05e86e9e962f86464
Azudeus <blitzthunder68@yahoo.com>
2016-10-08 16:22:13 +0700

    Fixed interface and putting value in text field

--- editproduct.php
+++ editproduct.php
@@ -55,30 +55,36 @@
      </table>
 </form>-->
 <?php
-$prodid = $_GET["edit"];
-$qName = "SELECT product_name FROM product WHERE product_id=".$prodid;
-$qDesc = "SELECT product_description FROM product WHERE product_id=".$prodid;
-$qPrice = "SELECT product_price FROM product WHERE product_id=".$prodid;
-$pName = $conn->query($qName);
-$pDesc = $conn->query($qDesc);
-$pPrice = $conn->query($qPrice);
-?>
-<div id = MyForm>
-        <span id = "fName"> Name </span><br>
-        <input type ="text" class ="inline" id ="Name" value = "<?php $pName?>></input><br>
-        <span id = "fDesc"> Description (max 200 chars) </span><br>
-        <input type ="text" id ="description value = <?php echo $pDesc ?>"><br>
-        <span id = "fPrice"> Price(IDR) </span><br>
-        <input type ="text" id ="price" value = <?php echo $pPrice ?>><br>
-        <span id = "pphoto"> Photo </span> <br>
-        <input type ="file" name= "photochoose">
-        <input type="hidden" id = "ausername" value="<?php $username?>"</p>
+$prodid = $_GET["product_id"];
+$query = "SELECT * from product where product_id=".$prodid;
+$q_result = $conn->query($query);
+
+while($row = $q_result-> fetch_assoc()) {
+    $pName = $row["product_name"];
+    $pDesc = $row["product_description"];
+    $pPrice = $row["product_price"];
+}
+
+echo "
+    <div id = 'formField'>
+    <form name='edit_product' action = '' method ='POST' onSubmit='return validateEdit()'>
+        Name <br>
+            <input type='text' name='name' id='formField' value='".$pName."'><br><br>
+        Description(max 200 chars) <br>
+            <textarea rows='3' cols='50' name='address' id='formField' value='".$pDesc."'></textarea><br><br>
+        Price (IDR)<br>
+            <input type='text' name='price' id='formField' value='".$pPrice."'><br><br>
+        Photo <br>
+            <input type ='file' name= 'photochoose'>
+        <input type='hidden' id = 'username' value='".$username."'</p>
      <table>
-         <td> <input type = "submit" id = "addbutton" value ="ADD" onclick="myProductValidate(true,<?php echo $prodid?>)"> 
-         <td> <input type = "button" id = "cancelbutton" value ="CANCEL" 
-                     onclick = "myProductCancel(<?php echo($activeuser) ?>)">
+         <td> <input type = 'submit' id = 'addbutton' value ='UPDATE'> </td>
+         <td> <input type = 'reset' id = 'cancelbutton' value ='CANCEL'
+                     onclick = 'myProductCancel(<?php echo($activeuser) ?>)'></td>
      </table>
-</div>
+</div>"
+
+?>
 </body>
 </html>
 
