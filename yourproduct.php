efc14befb1594cb9043b2110b8a431e3d26354e4
Azudeus <blitzthunder68@yahoo.com>
2016-10-08 21:18:21 +0700

    Removed unnecessary line

--- yourproduct.php
+++ yourproduct.php
@@ -34,25 +34,6 @@
 	if($q_result-> num_rows > 0){
 		//output data of each row from database
 		while($row = $q_result-> fetch_assoc()){
-		
-
-        // 	$phpdate = strtotime($row["product_datetime"]);
-		// 	$mysqldate1 = date('l, d F Y',$phpdate);
-		// 	$mysqldate2 = date('H.i',$phpdate);
-  //                       echo "<p id = 'date1'><span id = 'day1'> $mysqldate1 </span> <br> "
-  //                               . "<span id = 'clock1'> at $mysqldate2 </span><br> ";
-		// 	echo "<p id = 'product'><b>".
-  //                               "<span id = 'itemname'>" . $row["product_name"] . "</span><br>" .
-  //                               "<span id = 'price'> IDR " . $row["product_price"] . "</span><br>" .
-  //                               "<span id = 'description'>" . $row["product_description"] . "</span><br>" .
-  //                               "<span id = 'likes'>" . $row["likes"] ." likes <br>" . 
-  //                               "<span id = 'purchases'>" . $row["purchase"] ." purchases<br>" .
-  //                               "<div id ='edit'><a href='editproduct.php?account_id=".$account_id.
-  //                                   "&edit=".$row["product_id"]."'> EDIT </a> </div>". 
-  //                               "<div id = 'delete'><a href = 'deleteProduct.php?prod_id=".$row["product_id"]."'>"
-  //                               . "<onclick = alert('Are you sure?')> DELETE </div></div>";
-		// }
-
             $phpdate = strtotime($row["product_datetime"]);
             $mysqldate1 = date('l, d F Y',$phpdate);
             $mysqldate2 = date('H i',$phpdate);
